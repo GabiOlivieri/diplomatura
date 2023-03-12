fabddc1e2154ecfc8320ca8fd6a6f9463e2234fb
Gabriel Olivieri <gabi@DESKTOP-1LTQTKG.localdomain>
2023-03-12 15:36:24 -0300

    Añado rutas que varian segun lo que se manda por parametro

--- futbol/backend/src/app.js
+++ futbol/backend/src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 var nosotrosRouter = require('./routes/nosotros');
+const session = require('express-session');
+
+bodyParser = require('body-parser').json();
 
 let port = process.env.PORT;
 if(process.env.NODE_ENV === "test") port = 3000
@@ -8,14 +11,37 @@ const app = express();
 
 app.listen(port)
 
-app.get('/', function (req,res){
-    res.send('Hola mundo!');
-});
-
 app.route('/messi').get(function (req,res){
     res.send('Hola Messi!');
 }).post(function (req,res){
     res.send("Posteaste un messi");
 });
+
+app.use(session({
+    secret: 'ciclon2023',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.get('/', function (req,res){
+    if (req.session.nombre){
+        res.send('Hola ' + req.session.nombre);
+    }else{
+        res.send('Hola usuario desconocido.');
+    }
+});
+
+app.post('/ingresar', bodyParser, function (req,res){
+    if (req.body.nombre){
+        req.session.nombre = req.body.nombre;
+    }
+    res.redirect('/');
+});
+
+app.get('/salir', function (req,res){
+    req.session.destroy();
+    res.redirect('/');
+})
+
 
 app.use('/nosotros', nosotrosRouter);
